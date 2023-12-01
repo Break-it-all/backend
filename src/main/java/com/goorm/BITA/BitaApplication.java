35c7bc713c460c6233cec282eb31793efcf09cb5
wlstnam <wlstnam99@gmail.com>
2023-12-01 17:06:23 +0900

    init

---
+++ src/main/java/com/goorm/BITA/BitaApplication.java
@@ -0,0 +1,12 @@
+package com.goorm.BITA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitaApplication.class, args);
+	}
+}
