321c4d5f291a0b74b840f939f8d5632e47e724b6
hstla <ree7243@gmail.com>
2023-12-04 00:38:04 +0900

    cors 포트 추가.

--- src/main/java/com/goorm/BITA/config/WebConfig.java
+++ src/main/java/com/goorm/BITA/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("http://localhost", "http://localhost:3000") // 명시적 출처 지정
+            .allowedOriginPatterns("http://localhost", "http://localhost:3000", "http://localhost:8080")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
